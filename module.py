5077e2cbb3967901bb7bcf4517733d452e0f3691
Dharshini-457 <dharshinir619@gmail.com>
2025-01-16 22:45:54 +0530

    just a commit

--- module.py
+++ module.py
@@ -13,9 +13,3 @@ print(sentiment_scores)
 
 
 
-'''import 
-st.title("Sentiment Anslyzer kit!!")
-st.balloons()
-st.subheader("***Here we test the Emotion of the text that you have entered get ready with me...*** ")
-text= st.text_input("Enter the text....")
-if st.button("Test the Text!!"): '''
