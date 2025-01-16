91a21adc969b0bd1c97f929aa78d185da8ff967c
Dharshini-457 <dharshinir619@gmail.com>
2025-01-16 22:41:36 +0530

    New text analysis project commit

--- ml_pro.py
+++ ml_pro.py
@@ -1,13 +1,24 @@
 import streamlit as st
+import nltk 
+nltk.download('vader_lexicon')
+from nltk.tokenize import word_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+st.header("***WELCOME TO OUR PROJECT!!!***")
+st.subheader("***FEELING FINDER***")
+analyzer= SentimentIntensityAnalyzer()
+text=st.text_input("Enter a text to analysis....")
+text = word_tokenize(text)
+text=str(text)
+sentiment_scores = analyzer.polarity_scores(text) # Determine the sentiment
+if st.button("Apply"):
+    if sentiment_scores['compound'] >= 0.05: 
+        st.write( "Positive" )
+        st.balloons()
+    elif sentiment_scores['compound'] <= -0.05:
+        st.write( "Negative" )
+        st.balloons()
+    else: 
+        st.write( "Neutral")
+        st.balloons()
+ 
 
-from nltk.sentiment import SentimentIntensityAnalyzer as sia 
-def score(text):
-    score=sia.polarity_scores(text)
-    st.write(score) 
-    st.title("***Sentiment Scores!!***")
-    st.write(f" Your text {text}is Positive!! ")
-    st.write(f"Your text {text}is  Negative!!")
-    st.write(f"Your text{text} is  Neutral!!")
-    st.write(f"Your text {text} is Compound!!")
-    
-score("good day")
--- module.py
+++ module.py
@@ -1,4 +1,18 @@
 import streamlit as st
+import nltk 
+nltk.download('vader_lexicon')
+from nltk.tokenize import word_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+analyzer= SentimentIntensityAnalyzer()
+text = word_tokenize("NLTK is a great library for text processing!")
+text=(str(text))
+print(text)
+sentiment_scores = analyzer.polarity_scores(text)
+print(sentiment_scores)
+
+
+
+
 '''import 
 st.title("Sentiment Anslyzer kit!!")
 st.balloons()
