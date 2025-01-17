a8f0b3c46702dc44c5307c3c63beb55fc73042db
Dharshini-457 <dharshinir619@gmail.com>
2025-01-17 12:15:18 +0530

    new commit

--- ml_pro.py
+++ ml_pro.py
@@ -10,12 +10,13 @@ def result(text):
     analyzer= SentimentIntensityAnalyzer()
     sentiment_scores = analyzer.polarity_scores(text)
     if sentiment_scores['compound'] >= 0.05: 
-        st.write( "Positive" )
+        st.write( "Positive!!!" )
         st.balloons()
     elif sentiment_scores['compound'] <= -0.05:
-        st.write( "Negative" )
+        st.write( "Negative!!!" )
         st.balloons()
-   
+    else:
+        st.write("Neutral!!")
         st.balloons()
 text=st.text_input("Enter a text to analysis....")
 if st.button("Apply"):
